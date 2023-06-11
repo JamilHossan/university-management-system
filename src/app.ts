c511c6940466e879a816b5807be8f04487c1ee3e
unknown <https://github.com/Programming-Hero-Web-Course4/l2tsa1-basic-typescript-Topuhossain.git>
2023-06-11 23:16:52 +0600

    adding check

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 // const express = require('express')
-const app = express()
-const port = 3000
+const app = express();
 
 app.use(cors())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-export default app;
+export default app
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,15 @@
-
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
 async function fight() {
- 
-  try{
+  try {
     await mongoose.connect(config.database_url as string)
-    console.log(`database connection successfully`);
-    app.listen(config.port, () =>{
+    console.log(`database connection successfully`)
+    app.listen(config.port, () => {
       console.log(`the port is connected at port ${config.port}`)
     })
+  } catch (err) {
+    console.log('server is not connected', err)
   }
-  catch(err){
-    console.log('server is not connected',err)
-  }
-  
 }
-fight();
-
-
-
+fight()
